4743e80366173d0780547cd802ee0aaab59923b2
Dawid <dawid_plocharczyk3@o2.pl>
2021-03-21 21:21:31 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# Zad 1
+# Wygeneruj liczby podzielne przez 4 i zapisz je do pliku.
+# plik = open("zad1.txt","w+")
+# lista = [4, 8, 12, 16, 20]
+# plik.writelines(str(lista))
+# plik.close()
+
+# Zad 2
+# Odczytaj plik z poprzedniego zadania i wyświetl jego zawartość
+# with open("zad1.txt","r") as plik:
+#     for linia in plik:
+#         print(linia,end="")
+
+# Zad 3
+# Wykorzystując komendę with zapisz kilka linijek tekstu do pliku a następnie wyświetl je na ekranie.
+# with open("zad1.txt","r+") as plik:
+#     plik.write("\ndasdasdadsadsadsadsa")
+#     for a in plik:
+#         print(a,end="")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
